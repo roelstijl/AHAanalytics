7cba375d7dcd1a5ad8e8946590afe352401965d5
roelstijl <roel.stijl@bearingpoint.com>
2014-12-30 15:12:27 +0100

    Upgraded PC, XY and polygon+ NOR log

--- AHA_Data_BAR_GEOMETRY.R
+++ AHA_Data_BAR_GEOMETRY.R
@@ -1,49 +1,77 @@
-AHA_Data_BAR_GEOMETRY = function(mdsys,mode="polygons",atype="kabels"){
-  # MDsys is the collumn with the GEO-information ()
-  # Converts the following "MDSYS.SDO_GEOMETRY(2001,28992,MDSYS.SDO_POINT_TYPE(185462.693,436911.424,NULL),NULL,NULL)"
-  # Modes: 
-  # polygons (export plotable polygons)
-  # beginend (export the beginning and end of each polygon)
-  # position (export the avg location of the polygon)
-  #   cat("Starting cluster with multiple processor cores\n")
-  
-  #   tic();cl <<- makeCluster(getOption("cl.cores", 3));toc()
-  
-  cat("Extracting character\n"); tic()
-  mdsys = mdsys[!is.na(mdsys)]
-  mdsys = strsplit(mdsys, "\\(|\\)"); 
-  toc(); cat("Converting to data tables in list\n")
-  mdsys=switch(atype,
-              kabels = llply(mdsys, 
-              function(x) t(matrix(as.numeric(do.call(rbind, strsplit(x[7],","))),2,)) 
-              ,.progress="text"),
-              
-              moffen = alply(mdsys, 
-              function(x) t(matrix(as.numeric(strsplit(x[3],",")[[1]][1:2],2))) 
-              ,.progress="text")); 
-  
-  names(mdsys) = as.character(1:length(mdsys))
-  mdsys=mdsys[(which(!laply(mdsys,functany(is.na(x)))))]
-  #   stopCluster(cl)
-  
-  cat("Converting to output\n"); tic()
-  nu<<-1
-  mdsys= switch(mode,
-                polygons = llply(mdsys,function(x) 
-                {nu<<-nu+1; Polygons(list(Polygon(rbind(x,x[(nrow(x)-1):1,]))),as.character(nu))},.progress="text"),
-                beginend = llply(mdsys,function(x) cbind(x[1,],x[nrow(x),]),.progress="text"),
-                )
-  
-  toc(); cat("Converting to final\n"); tic()
-  mdsys = switch(mode,
-                 polygons = SpatialPolygons(mdsys,proj4string=CRS("+init=epsg:28992")),
-                 beginend = data.table(t(matrix(unlist(mdsys),4,length(mdsys)))),
-                 position = data.table(t(matrix(unlist(mdsys),2,length(mdsys)))))
-  
-  a=switch(mode,
+AHA_Data_BAR_GEOMETRY = function(mdsysori,mode="polygons",atype="kabels",DT = "none"){
+# MDsys is the collumn with the GEO-information ()
+# Converts the following "MDSYS.SDO_GEOMETRY(2001,28992,MDSYS.SDO_POINT_TYPE(185462.693,436911.424,NULL),NULL,NULL)"
+# Modes: 
+# polygons (export plotable polygons)
+# beginend (export the beginning and end of each polygon)
+# position (export the avg location of the polygon)
+
+# Settings --------------------------
+cat(paste0("\n Started: ",as.character(Sys.time()),"\n"))
+
+noloops   = 20
+loopblock = ceil(seq(0.000000001,noloops,length.out=length(mdsysori)))
+pb        = tkProgressBar (title = "Import", label = "Starting...", min = 0, max = noloops, initial = 0, width = 450); pc=0;
+mdsysout = list()
+nu<<-1
+
+# Loop over the values in order to prevent memory issues -----------------
+  for (loopy in 1:noloops)
+{
+    mdsys = mdsysori[loopblock==loopy]
+    setTkProgressBar (pb, loopy, 
+                      title = paste0("AHA_Data_BAR_GEOMETRY, loop: ", loopy," of ", noloops, " n=", length(mdsys)),
+                      label = "Splitting strings"); 
+        
+    mdsys = mdsys[!is.na(mdsys)]
+    mdsys = strsplit(mdsys, "\\(|\\)"); 
+    setTkProgressBar (pb, loopy,label = "Converting to data tables in list");
+    
+    # Extract the coordinated from the character array
+    mdsys=switch(atype,
+                 kabels = {temp = llply(mdsys,function(x) t(matrix(as.numeric(do.call(rbind, strsplit(x[7],","))),2,)))
+                           names(temp) = as.character(1:length(temp))
+                           temp[which(!laply(temp,function(x) any(is.na(x))))]},
+                 
+                 moffen = laply(mdsys,function(x) t(matrix(as.numeric(strsplit(x[3],",")[[1]][1:2],2))))
+                 ); 
+    
+    setTkProgressBar (pb, loopy,label = "Converting to selected output"); 
+
+# Preprocess the geospatial data --------------------------
+    
+    mdsys= switch(mode,
+                  polygons = llply(mdsys,function(x) 
+                  {Polygons(list(Polygon(rbind(x,x[(nrow(x)-1):1,]))),as.character(nu));
+                   nu<<-nu+1}),
+                  
+                  beginend = llply(mdsys,function(x) cbind(x[1,],x[nrow(x),])),
+                  position = mdsys,
+                  points   = mdsys)
+    
+# Convert it to geospatial data --------------------------
+    setTkProgressBar (pb, loopy,label = "Converting to geospatial output");
+    mdsys = switch(mode,
+                   polygons = SpatialPolygons(mdsys,proj4string=CRS("+init=epsg:28992")),
+                   points   = {ifelse(is.character(DT),
+                                      {temp = data.table(Index = nu:(nu-1+nrow(mdsys)))},
+                                      {temp = DT[nu:(nu-1+nrow(mdsys))]})
+                               nu<<-nrow(mdsys)+1
+                               temp = temp[!is.na(mdsys[,1]),]
+                               coordinates(temp) = mdsys[!is.na(mdsys[,1]),]
+                               proj4string(temp) = CRS("+init=epsg:28992")
+                               temp},
+                   beginend = data.table(t(matrix(unlist(mdsys),4,length(mdsys)))),
+                   position = data.table(mdsys))
+    
+    a  =  switch(mode,
              beginend = {setnames(mdsys,c("Coo_X_van","Coo_Y_van","Coo_X_naar","Coo_Y_naar"))},
              position = {setnames(mdsys,c("Coo_X","Coo_Y"))})
-  toc()
+    mdsysout[[loopy]] = mdsys
+  }
+  
+  setTkProgressBar (pb, loopy,label = "Done");
+
   #   map= plotGoogleMaps(finaloutput,legend=FALSE,strokeColor = "Blue",strokeWeight = 100)
-  return(mdsys)
+  return(do.call(rbind,mdsysout))
 }
