6e60e752da939b0b090ae59e5870c28684282bf8
unknown <AL5696@D-AW15BX.alliander.local>
2015-02-13 09:50:41 +0100

    Verder werken samenvoegen proxies, fixen rbindlist.

--- AHA_Proxy_samenv.R
+++ AHA_Proxy_samenv.R
@@ -1,14 +1,15 @@
 proxy_samenv  <- function(global=F){
   if(global){
-    if(!(exists(proxy_res))){
+    if(!(exists("proxy_res"))){
       setwd(settings$Analyse_Datasets)
       proxy_res      <- list() 
-      myFile <- file.choose(); load(myFile);
+       myFile <- file.choose(); load(myFile);
       proxy_res$PC   <- assetsltb
-      myFile <- file.choose(); load(myFile);
+       myFile <- file.choose(); load(myFile);
       proxy_res$XY   <- assetsltb
-      myFile <- file.choose(); load(myFile);
+       myFile <- file.choose(); load(myFile);
       proxy_res$TOPO <- assetsltb
+      proxy_res <<- proxy_res
       rm(assetsltb)
     }
   }else{
@@ -26,14 +27,17 @@ proxy_samenv  <- function(global=F){
   if(!exists("storingen")){load(paste0(settings$Input_Datasets,"/1. AID KID proxy/AHA_Proxy_partial_data_storingen.Rda"))}
   
   koppellijst =list()
-  cbind(proxy_res$PC$LSkabels[["3835410"]],ID_KLAK_Melding=c("3835410"))
-  names(proxy_res$PC$LSkabels[["3835410"]])
+  
   for(klasse in c("LSkabels","LSmoffen","MSmoffen","MSkabels")){
     
-    koppellijst[klasse] <- rbind(proxy_res$PC[klasse],proxy_res$XY[klasse],proxy_res$TOPO[klasse])
+    koppellijst[klasse] <- rbind(cbind(rbindlist(proxy_res$PC[[klasse]][which(ldply(proxy_res$PC[[klasse]],nrow)$V1>0)]),method="PC"),
+                                 cbind(rbindlist(proxy_res$XY[[klasse]][which(ldply(proxy_res$XY[[klasse]],nrow)$V1>0)]),method="XY"),
+                                 cbind(rbindlist(proxy_res$TOPO[[klasse]][which(ldply(proxy_res$TOPO[[klasse]],nrow)$V1>0)]),method="TOPO"))
       
     print("klaar")
   }
   
   
 }
+
+
