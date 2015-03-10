2f44cf7e096e5f56d00c71d13a0e60fb7f1ff136
unknown <roel.stijl@NLAMS4043734Z.net.work>
2015-03-10 10:35:39 +0100

    Commit

--- AHA_Proxy_KA_Preprocessing.R
+++ AHA_Proxy_KA_Preprocessing.R
@@ -4,7 +4,7 @@ AHA_Data_KA_Proxy_Preprocessing = function(datasets=c("assetsBAR","assetsNOR","n
 # cfg$firstdate_NOR is the startdate of the data subset
 # last date is the last date to take
 #
-# Settings  ----------------------
+# Settings --------------------------------
 cfg = list()
 cfg$pb  = pbarwrapper(title = paste0("AHA_Data_KA_Proxy_Preprocessing: ",as.character(Sys.time())),label = "Start", min = 0, max = 3*length(datasets)+1);
 
@@ -13,7 +13,7 @@ cfg$firstdate_NOR = as.Date("2007-02-01")
 cfg$firstdate_BAR = as.Date("2014-02-14")
 cfg$lastdate      = as.Date("2015-02-01")
 
-# Choose the correct functions 
+# Choose the correct functions --------------------
 switch (datasets,
         assetsBAR = AHA_assetsBAR(cfg),
         assetsNOR = AHA_assetsNOR(cfg),
@@ -26,9 +26,8 @@ switch (datasets,
 setpbarwrapper(cfg$pb, label = "Done");
 }
 
-# Different functions for the dataset calculations----------------------------
-AHA_assetsBAR = function(cfg){        
-# BAR Data ----------------------------------------------------------------
+AHA_assetsBAR = function(cfg){   
+# BAR Data -------------------------------
 setpbarwrapper(cfg$pb, label = "Loading BAR data");
 # Laad de assets en converteer de datums als deze verkeerd staan 
 load(paste0(settings$Input_Datasets,"/2. All Assets/Asset_Data_BAR_assets.Rda"))
@@ -36,29 +35,34 @@ assets$MSHLDROUTE = NULL
 
 setpbarwrapper(cfg$pb, label = "Calculating BAR data");
 # behoud alleen dat deel van de assets dat binnen de periode veranderd is.
-assets = lapply(assets,function(x)
-{x[!(x$DateAdded == cfg$firstdate_BAR & x$Status_ID=="Active")]})
-  
-minassets$MSkabels=assets$MSkabels[(!is.na(DateAdded)& DateAdded > cfg$firstdate_NOR & DateAdded < cfg$lastdate) | 
-                                 (!is.na(DateRemoved)& DateRemoved > cfg$firstdate_NOR & DateRemoved < cfg$lastdate )]
-minassets$LSkabels=assets$LSkabels[(!is.na(DateAdded)& DateAdded > cfg$firstdate_NOR & DateAdded < cfg$lastdate ) | 
-                                 (!is.na(DateRemoved)& DateRemoved > cfg$firstdate_NOR & DateRemoved < cfg$lastdate )]
 
-minassets$MSmoffen=assets$MSmoffen[(!is.na(DateAdded)& DateAdded > cfg$firstdate_NOR & DateAdded < cfg$lastdate) | 
-                                 (!is.na(DateRemoved)& DateRemoved > cfg$firstdate_NOR & DateRemoved < cfg$lastdate)]
-minassets$LSmoffen=assets$LSmoffen[(!is.na(DateAdded)& DateAdded > cfg$firstdate_NOR & DateAdded < cfg$lastdate) | 
-                                 (!is.na(DateRemoved)& DateRemoved > cfg$firstdate_NOR & DateRemoved < cfg$lastdate)]
+minassets = list();
 
+minassets$LSkabels=assets$LSkabels[(!is.na(DateAdded)& DateAdded > cfg$firstdate_BAR & DateAdded < cfg$lastdate) | 
+                                 (!is.na(DateRemoved)& DateRemoved > cfg$firstdate_BAR & DateRemoved < cfg$lastdate)|
+                                 (!is.na(DateStatus_ch)& DateStatus_ch > cfg$firstdate_BAR & DateStatus_ch < cfg$lastdate & substrRight(Status_ch,11)=="Uit Bedrijf")|
+                                 (!is.na(DateLength_ch)& DateLength_ch > cfg$firstdate_BAR & DateLength_ch < cfg$lastdate)]
+
+minassets$MSkabels=assets$MSkabels[(!is.na(DateAdded)& DateAdded > cfg$firstdate_BAR & DateAdded < cfg$lastdate) | 
+                                     (!is.na(DateRemoved)& DateRemoved > cfg$firstdate_BAR & DateRemoved < cfg$lastdate)|
+                                     (!is.na(DateStatus_ch)& DateStatus_ch > cfg$firstdate_BAR & DateStatus_ch < cfg$lastdate & substrRight(Status_ch,11)=="Uit Bedrijf")|
+                                     (!is.na(DateLength_ch)& DateLength_ch > cfg$firstdate_BAR & DateLength_ch < cfg$lastdate)]
+
+minassets$MSmoffen=assets$MSmoffen[(!is.na(DateAdded)& DateAdded > cfg$firstdate_BAR & DateAdded < cfg$lastdate) | 
+                                 (!is.na(DateRemoved)& DateRemoved > cfg$firstdate_BAR & DateRemoved < cfg$lastdate)]
+minassets$LSmoffen=assets$LSmoffen[(!is.na(DateAdded)& DateAdded > cfg$firstdate_BAR & DateAdded < cfg$lastdate) | 
+                                 (!is.na(DateRemoved)& DateRemoved > cfg$firstdate_BAR & DateRemoved < cfg$lastdate)]
+
+assets = minassets
 
 # Opsplitsen in MS en LS, zo zit het in de BARlog ook
 setpbarwrapper(cfg$pb, label = "Saving BAR data");
-save(assets,file=paste0(settings$Input_Datasets,"/1. AID KID proxy/AHA_Proxy_partial_data_assets_BAR.Rda"))
-remove("assets")
-
+save(assets,file=paste0(settings$Input_Datasets,"/1. AID KID proxy/AHA_Proxy_partial_data_assets_BAR.Rda"),compress=F)
 }
+
 AHA_assetsNOR = function(cfg){        
 
-# NOR Data ----------------------------------------------------------------
+# NOR Data
 setpbarwrapper(cfg$pb, label = "Loading NOR data");
 
 # Set what dates unexplainable bumps in the data occured, NOR
@@ -67,13 +71,11 @@ cfg$BadDates$kabels = list(
   DateAdded   = as.Date(c("2007-01-06","2010-07-03",'2011-02-05', "2011-06-04","2012-05-05")),
   DateRemoved = as.Date(c("1970-01-01","2007-01-06","2010-07-03",'2011-02-05', "2011-06-04","2012-05-05")),
   Date_Status_ch = as.Date(c("1970-01-01","2012-12-08")),
-  DateLength_ch = as.Date("1970-01-01"))
+  DateLength_ch = as.Date("1970-01-01","2015-01-03","2012-12-08","2007-11-03","2011-02-05","2007-09-08"))
 
 cfg$BadDates$moffen = list(
-  DateAdded   = as.Date(c("2007-01-06","2010-07-03",'2011-02-05', "2011-06-04","2012-05-05")),
-  DateRemoved = as.Date(c("1970-01-01","2007-01-06","2010-07-03",'2011-02-05', "2011-06-04","2012-05-05")))
-
-cfg$BadDates$Verbindingen = as.Date(ymd(c("2007-01-06","2011-08-01","2011-04-06","2012-05-05")))
+  DateAdded   = as.Date(c("2007-01-06","2011-06-04","2011-02-05","2010-07-03",'2011-02-05', "2011-06-04","2012-05-05")),
+  DateRemoved = as.Date(c("1970-01-01","2007-01-06","2010-07-03",'2011-02-05', "2011-06-04","2012-05-05","2011-08-06")))
 
 # Laad de assets en converteer de datums als deze verkeerd staan 
 load(paste0(settings$Input_Datasets,"/2. All Assets/Asset_Data_NOR_assets.Rda"))
@@ -90,7 +92,7 @@ assets$kabels$PC_4_naar= substr(assets$kabels$PC_6_naar,1,4)
 minassets = list()
 minassets$kabels=assets$kabels[(!is.na(DateAdded)& DateAdded > cfg$firstdate_NOR & DateAdded < cfg$lastdate & !(DateAdded %in% cfg$BadDates$kabels$DateAdded)) | 
                                  (!is.na(DateRemoved)& DateRemoved > cfg$firstdate_NOR & DateRemoved < cfg$lastdate & !(DateRemoved %in% cfg$BadDates$kabels$DateRemoved))|
-                                 (!is.na(Date_Status_ch)& Date_Status_ch > cfg$firstdate_NOR & Date_Status_ch < cfg$lastdate & !(Date_Status_ch %in% cfg$BadDates$kabels$Date_Status_ch))|
+                                 (!is.na(Date_Status_ch)& Date_Status_ch > cfg$firstdate_NOR & Date_Status_ch < cfg$lastdate & !(Date_Status_ch %in% cfg$BadDates$kabels$Date_Status_ch) & substrRight(Status_ch, 14)=="Buiten Bedrijf")|
                                  (!is.na(DateLength_ch)& DateLength_ch > cfg$firstdate_NOR & DateLength_ch < cfg$lastdate & !(DateLength_ch %in% cfg$BadDates$kabels$DateLength_ch))]
 
 minassets$moffen=assets$moffen[(!is.na(DateAdded)& DateAdded > cfg$firstdate_NOR & DateAdded < cfg$lastdate & !(DateAdded %in% cfg$BadDates$moffen$DateAdded)) | 
@@ -111,8 +113,9 @@ save(assets,file=paste0(settings$Input_Datasets,"/1. AID KID proxy/AHA_Proxy_par
 remove("assets")
 
 }
+
 AHA_nettopo   = function(cfg){ 
-# EAN-Hoofdleiding-XY-PC data ----------------
+# EAN-Hoofdleiding-XY-PC data
 nettopo = list();  
 
 # Load the data
@@ -131,22 +134,33 @@ load(paste0(settings$Ruwe_Datasets,"/24. Adressendichtheid/Count Adresses.Rda"))
 adressendichtheid = mindataset
 load(paste0(settings$Ruwe_Datasets,"/10. BAG/PC_4_Spatial.Rda"))
 pc4area = data.table(pc4@data[,c("PC4CODE","SHAPE_AREA")]);
+
+# Load BAR data for Verbindingen of Kabels
+load(paste0(settings$Ruwe_Datasets,"/1. BARlog/MH_NRG_LS_KABELS_XY.Rda"))
+BAR_LS_Kabels = mindataset[,list(ID_Verbinding, ID_NAN,ID_Hoofdleiding,ID_Kabel)]
+
 rm("mindataset")
 
 # Start calculating the nettopology
+# Add the ID_Verbinding
+setkey(EAN_Aansluitingen,ID_Kabel)
+setkey(BAR_LS_Kabels,ID_Kabel)
+EAN_Aansluitingen[,ID_Verbinding:=unique(BAR_LS_Kabels)[EAN_Aansluitingen,ID_Verbinding]]
+
+# Combine the sets
 setkey(Hoofdleiding_Station,ID_EAN)
 setkey(Hoofdleiding_Station2,ID_EAN)
 setkey(EAN_Aansluitingen,ID_EAN)
 
 setpbarwrapper(cfg$pb, label = "Calculating Nettopo data");
 nettopo = unique(Hoofdleiding_Station)[unique(EAN_Aansluitingen),
-                                       list(ID_EAN,ID_Hoofdleiding,ID_Station,ID_Kabel,Naam_Ruimte,ID_NR_Ruimte,
+                                       list(ID_EAN,ID_Hoofdleiding,ID_Station,ID_Kabel,ID_Verbinding,Naam_Ruimte,ID_NR_Ruimte,
                                             ID_Stationbehuizing,Coo_X,Coo_Y,Adres,SJV,SJV_Laag,Profiel_Type)]
 
 set(nettopo,which(is.na(nettopo$ID_Hoofdleiding)),j=1:ncol(nettopo),
     unique(Hoofdleiding_Station2)[unique(
       EAN_Aansluitingen)[is.na(nettopo$ID_Hoofdleiding)],
-      list(ID_EAN,ID_Hoofdleiding,ID_Station,ID_Kabel,Naam_Ruimte,ID_NR_Ruimte,
+      list(ID_EAN,ID_Hoofdleiding,ID_Station,ID_Kabel,Naam_Ruimte,ID_Verbinding,ID_NR_Ruimte,
            ID_Stationbehuizing,Coo_X,Coo_Y,Adres,SJV,SJV_Laag,Profiel_Type)])
 
 # Add the MS routes
@@ -170,78 +184,115 @@ nettopo = pc4area[nettopo]
 
 # Save
 setpbarwrapper(cfg$pb, label = "Saving Nettopo data");
-save(nettopo,file=paste0(settings$Input_Datasets,"/1. AID KID proxy/AHA_Proxy_partial_data_nettopo_EAN.Rda"))
+save(nettopo,file=paste0(settings$Input_Datasets,"/1. AID KID proxy/AHA_Proxy_partial_data_nettopo_EAN.Rda"),compress=F)
 }
+
 AHA_storingen = function(cfg){
-# Storingsdata uit KLAK ------------------------
+# Storingsdata uit KLAK
 
 storingen=list()  
 setpbarwrapper(cfg$pb, label = "Loading KLAK data");
 
-# Laad de data om adressen te koppelen
+# Load the date required and do some basic operations ---------------------
+# Load the LS KLAK
 load(paste0(settings$Ruwe_Datasets,"/4. KLAK/KLAK_LS.Rda"))
 mindataset[,Datum:=as.Date(mindataset$Datum)]
-storingen$LS= mindataset[(mindataset$Datum > cfg$firstdate_NOR & mindataset$Datum < cfg$lastdate)]
+storingen$LS= unique(mindataset[(mindataset$Datum > cfg$firstdate_NOR & mindataset$Datum < cfg$lastdate)],by="ID_KLAK_Melding")
 storingen$LS[,Brontabel := "LS storingen"]
 
+# Add PC6 based coordinates
+load(paste0(settings$Ruwe_Datasets,"/4. KLAK/KLAK_PC_6_met_Coo.Rda"))
+PC_6_Coo = mindataset
+
+# Load the MS KLAK
 load(paste0(settings$Ruwe_Datasets,"/4. KLAK/KLAK_MS.Rda"))
 mindataset[,Datum:=as.Date(mindataset$Datum)]
 storingen$MS= mindataset[(mindataset$Datum > cfg$firstdate_NOR & mindataset$Datum < cfg$lastdate)]
 storingen$MS[,Brontabel := "MS storingen"]
 
+# Add XY based on the lon lat in the systems (converted to XY elsewhere)
+load(paste0(settings$Ruwe_Datasets,"/4. KLAK/KLAK MS en klant melding_XY.Rda"))
+try(setnames(mindataset,"KLAK_MELDING","ID_KLAK_Melding"))
+setkey(mindataset,ID_KLAK_Melding)
+setkey(storingen$MS,ID_KLAK_Melding)
+storingen$MS = mindataset[,list(ID_KLAK_Melding,Coo_X,Coo_Y)][storingen$MS]
+
+# Load Meldingen
 load(paste0(settings$Ruwe_Datasets,"/4. KLAK/KLAK_KOPPEL_MELDING_GROEP.Rda"))
 storingen$KLAKMelders = (mindataset);
 storingen$KLAKMelders[,Brontabel := "Melder"]
-                                            
+                
+# Load GISmutaties
 load(paste0(settings$Ruwe_Datasets,"/21. GIS-mutaties/GISMUTATIE.Rda"))
 setkey(mindataset,ID_KLAK_Melding)
 gismutaties = mindataset
 
-setpbarwrapper(cfg$pb, label = "Loading Nettoplogy data");
+# Load nettopology data
 load(paste0(settings$Input_Datasets,"/1. AID KID proxy/AHA_Proxy_partial_data_nettopo_EAN.Rda"))
 
 load(paste0(settings$Ruwe_Datasets,"/11. Nettopologie/MS_Stations.Rda")); 
 MS_Stations = mindataset
 
-# Add and correct Postcodes
-setpbarwrapper(cfg$pb, label = "Process KLAK data");
+load(paste0(settings$Ruwe_Datasets,"/11. Nettopologie/MS_hoofdleidingen.Rda")); 
+MS_hoofdleidingen = mindataset
 
+# Correct the data ---------------------------------
+setpbarwrapper(cfg$pb, label = "Process KLAK data");
 storingen$LS[,Huisnr:=(onlynum(Adres))]
 
-setkey(storingen$LS,ID_KLAK_Melding)
-setkey(storingen$KLAKMelders,ID_KLAK_Melding)
+# Correct the PC with spaces and add PC4
+l_ply(names(storingen),function(x) storingen[[x]][,PC_6 :=gsub(" ","",PC_6)])
+l_ply(names(storingen),function(x) storingen[[x]][,PC_4 :=substr(PC_6,1,4)])
 
-storingen$MS[,Huisnr := as.numeric(NA)] 
-storingen$MS[,Coo_X := as.numeric(NA)] 
-storingen$MS[,Coo_Y := as.numeric(NA)] 
-storingen$MS[,ID_Hoofdleiding := as.character(NA)] 
-storingen$MS[,Routenaam := as.character(NA)] 
+# Add coordinates based on center of PC_6
+setkey(PC_6_Coo,PC_6)
+setkey(storingen$LS,PC_6)
+storingen$LS = PC_6_Coo[,list(PC_6,Coo_X_PC_6,Coo_Y_PC_6)][storingen$LS]
 
-storingen$MS[,PC_6 := gsub(" ","",PC_6)] 
-storingen$MS[,PC_4:=substr(storingen$MS$PC_6,1,4)]
+# Add the ms routenamen based on sections
+MS_Stations[,Naam_Onderstation:=gsub("150/20KV |50KV |SH |SS | 10KV| \\(Normaal\\)|RS |OS ", "",ID_Voedend_Station)]
+toremove = " |,|-|\\.|\\'"
+MS_Stations[,Sectie:=gsub(toremove,"",tolower(Lokale_naam))]
 
-storingen$LS[,PC_6 := gsub(" ","",PC_6)] 
-storingen$LS[,PC_4:=substr(storingen$LS$PC_6,1,4)]
-
-storingen$KLAKMelders[,PC_6 := gsub(" ","",PC_6)] 
-storingen$KLAKMelders[,PC_4 := substr(PC_6,1,4)]
-
-# Add the ms hoofdleidingen based on the sections
-MS_Stations[,Naam_Onderstation:=gsub("150/20KV |50KV |SH |SS | 10KV| \\(Normaal\\)|RS |OS ", "",Naam_voedend_station)]
-MS_Stations[,Sectie:=Lokale_naam]
-
-storingen$Routenamen = storingen$MS[,list(Naam_Onderstation,ID_KLAK_Melding)]
-storingen$Routenamen = data.table(Naam_Onderstation=storingen$MS$Naam_Onderstation,
+storingen$Nettopologie = data.table(Naam_Onderstation=storingen$MS$Naam_Onderstation,
                                   ID_KLAK_Melding = storingen$MS$ID_KLAK_Melding,
-                                  Sectie=c(storingen$MS$Sectie_naar,storingen$MS$Sectie_van))
+                                  Naam_Station_Van = gsub(toremove,"",tolower(c(storingen$MS$Sectie_van))),
+                                  Naam_Station_Naar = gsub(toremove,"",tolower(c(storingen$MS$Sectie_naar))),
+                                  Sectie=gsub(toremove,"",tolower(c(storingen$MS$Sectie_naar,storingen$MS$Sectie_van))))
+
+storingen$Nettopologie[Sectie==""| Sectie=="unset",Sectie:=NA]
 
 setkey(MS_Stations,Sectie)
-setkey(storingen$Routenamen,Sectie)
+setkey(storingen$Nettopologie,Sectie)
+storingen$Nettopologie = unique(MS_Stations[,list(Sectie,Routenaam)])[storingen$Nettopologie]
+setkey(storingen$Nettopologie,ID_KLAK_Melding,Routenaam)
 
-storingen$Routenamen = MS_Stations[,list(Sectie,Routenaam)][storingen$Routenamen,allow.cartesian=TRUE]
+# Add the hoofdleidingen based on sections
+station_namen_naar = laply(MS_hoofdleidingen$Naar_Station_B,
+      function(x){
+        (matrix(switch(ifelse(length(unlist(strsplit(x,"/")))==2,"two","nope"),
+                       two = unlist(strsplit(x,"/")),
+                       nope=c(NA,NA)
+        )        )        )      })
 
-setkey(storingen$Routenamen,ID_KLAK_Melding,Routenaam)
-storingen$Routenamen= unique(storingen$Routenamen)
+station_namen_van = laply(MS_hoofdleidingen$Van_Station_A,
+                           function(x){
+                             (matrix(switch(ifelse(length(unlist(strsplit(x,"/")))==2,"two","nope"),
+                                            two = unlist(strsplit(x,"/")),
+                                            nope=c(NA,NA)
+        )        )        )      })
+
+MS_hoofdleidingen[,Naam_Station_Van := gsub(toremove,"",tolower(station_namen_van[,2]))]
+MS_hoofdleidingen[,Naam_Station_Naar := gsub(toremove,"",tolower(station_namen_naar[,2]))]
+MS_hoofdleidingen[,Nummer_Station_Van := gsub(toremove,"",tolower(station_namen_van[,1]))]
+MS_hoofdleidingen[,Nummer_Station_Naar := gsub(toremove,"",tolower(station_namen_naar[,1]))]
+
+setkey(MS_hoofdleidingen,Naam_Station_Van,Naam_Station_Naar)
+setkey(storingen$Nettopologie,Naam_Station_Van,Naam_Station_Naar)
+
+storingen$Nettopologie[, ID_Hoofdleiding := MS_hoofdleidingen[storingen$Nettopologie,ID_Hoofdleiding]]
+storingen$Nettopologie[, ID_Verbinding   := MS_hoofdleidingen[storingen$Nettopologie,ID_Verbinding]]
+
 
 # Correct the KLAK meldingen, want alleen eerste melders tellen
 setnames(storingen$KLAKMelders,"ID_KLAK_Melding","ID_KLAK_Melding_oud")
@@ -253,6 +304,8 @@ setkey(temp,ID_Groep)
 storingen$KLAKMelders=temp[storingen$KLAKMelders]
 
 # Pak alleen relevante meldingen
+setkey(storingen$LS,ID_KLAK_Melding)
+setkey(storingen$KLAKMelders,ID_KLAK_Melding)
 storingen$KLAKMelders=storingen$KLAKMelders[(storingen$KLAKMelders$ID_KLAK_Melding %in% storingen$MS$ID_KLAK_Melding) | (storingen$KLAKMelders$ID_KLAK_Melding %in% storingen$LS$ID_KLAK_Melding)]
 
 # Bepaal het aantal melders  
@@ -272,8 +325,9 @@ setkey(nettopo,PC_6,Huisnr)
 storingen$KLAKMelders = unique(nettopo)[storingen$KLAKMelders]
 storingen$LS = unique(nettopo)[storingen$LS]
 
-save(storingen,file=paste0(settings$Input_Datasets,"/1. AID KID proxy/AHA_Proxy_partial_data_storingen.Rda"))
+save(storingen,file=paste0(settings$Input_Datasets,"/1. AID KID proxy/AHA_Proxy_partial_data_storingen.Rda"),compress=F)
 }
+
 AHA_validatie = function(cfg){
 # Validatieset van de storingsregistratoren-----------------------------------
 
--- AHA_Temp_PC6_inspection.R
+++ AHA_Temp_PC6_inspection.R
@@ -1,4 +1,15 @@
+# Take original BAR set
+load("E:/1. Alliander/3. Asset Health Analytics/1. Ruwe Datasets/1. BARlog/MH_NRG_MS_KABELS_XY_PC6.Rda")
+BAR_LS_Kabels = mindataset
+
+load("E:/1. Alliander/3. Asset Health Analytics/1. Ruwe Datasets/6. NOR/ELCVERBINDINGSDELEN_1501.Rda")
 NOR_source = mindataset
+
+load("E:/1. Alliander/3. Asset Health Analytics/2. Input Datasets/6. NOR/masterdataset_ELCVERBINDINGSDELEN_XY_PC6.Rda")
+NOR_PC = masterdataset
+
+load("E:/1. Alliander/3. Asset Health Analytics/2. Input Datasets/6. NOR/masterdataset_ELCVERBINDINGSDELEN.Rda")
+NOR_old = masterdataset
 
 NOR_source = NOR_source[!is.na(ID_NAN)]
 NOR_old = NOR_old[!is.na(ID_NAN)]
@@ -14,32 +25,25 @@ NOR_old       = unique(NOR_old)
 NOR_PC        = unique(NOR_PC)
 BAR_LS_Kabels = unique(BAR_LS_Kabels)
 
+setnames(NOR_PC,c("PC_6_van","Coo_X_van","Coo_Y_van"),c("PC_6_x","Coo_X_x","Coo_Y_x"))
+
+setnames(NOR_PC,c("PC_6_naar","Coo_X_naar","Coo_Y_naar"),c("PC_6_van","Coo_X_van","Coo_Y_van"))
+
 colren = c("PC_6_van","Coo_X_van","Coo_Y_van")
 setnames(NOR_source,colren,paste0(colren,"_","NOR_source"))
 setnames(NOR_old,colren,paste0(colren,"_","NOR_old"))
 setnames(NOR_PC,colren,paste0(colren,"_","NOR_PC"))
 setnames(BAR_LS_Kabels,colren,paste0(colren,"_","BAR_LS_Kabels"))
 
-sapply(setnames())
-
 PCXY = BAR_LS_Kabels[,list(ID_NAN,PC_6_van_BAR_LS_Kabels,Coo_X_van_BAR_LS_Kabels,
                            Coo_Y_van_BAR_LS_Kabels)][NOR_old[,list(PC_6_van_NOR_old,Coo_X_van_NOR_old,
                                                      Coo_Y_van_NOR_old,ID_NAN)]][NOR_PC[,list(PC_6_van_NOR_PC,Coo_X_van_NOR_PC,
                                                                                  Coo_Y_van_NOR_PC,ID_NAN)]][NOR_source[,list(PC_6_van_NOR_source,Coo_X_van_NOR_source,
                                                                                                                        Coo_Y_van_NOR_source,ID_NAN)]]
+setcolorder(PCXY, cn(PCXY))
 
 row.sample(PCXY[,list(PC_6_van_BAR_LS_Kabels,PC_6_van_NOR_old, PC_6_van_NOR_PC, PC_6_van_NOR_source)],100)
 
-write.xlsx(row.sample(PCXY,100),file="Postcode_tests.xlsx")
+write.csv(row.sample(PCXY,1000),file="Postcode_tests.csv")
 
 
-renamestuff = function (data,addition)
-  setnames(data,x,paste0(names(x),"_",addition))
-)
-
-# Take original BAR set
-load("E:/1. Alliander/3. Asset Health Analytics/1. Ruwe Datasets/1. BARlog/MH_NRG_LS_KABELS.Rda")
-BAR_source = mindataset
-
-load("E:/1. Alliander/3. Asset Health Analytics/1. Ruwe Datasets/6. NOR/ELCVERBINDINGSDELEN_1501.Rda")
-NOR_source = mindataset
